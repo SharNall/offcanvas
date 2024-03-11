19fdfaada63c50f3813915adf380523db26f2937
Mohammad Abu Saleh <146670958+SharNall@users.noreply.github.com>
2024-03-11 23:34:29 +0200

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector(".btn2");
+    btn.addEventListener('click',()=>{
+            document.querySelector(".overlay").classList.toggle("show")
+    })
+
+document.querySelector(".btn1").addEventListener('click',()=>{
+    document.querySelector(".overlay").classList.toggle("show");
+})
